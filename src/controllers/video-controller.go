fd38630494de3ce016c6e115ffdeeac5ea0ac89d
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-25 18:10:57 -0800

    add controllers

--- src/controllers/video-controller.go
+++ src/controllers/video-controller.go
@@ -1,4 +1,11 @@
 package controllers
 
+import (
+	"examples.com/entity"
+	"github.com/gin-gonic/gin"
+)
+
 type VideoController interface {
+	FindAll() []entity.Video
+	Save(ctx *gin.Context)
 }
--- src/entity/video.go
+++ src/entity/video.go
@@ -1,6 +1,7 @@
 package entity
 
 type Video struct {
+	//hows videos should be stored
 	Title       string `json: "title"`
 	Description string `json: "description"`
 	URL         string `json: "url"`
