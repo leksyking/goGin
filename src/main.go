844da8139f8049399c615a6c51d578ce3318311a
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-25 18:50:49 -0800

    handle routes and all

--- src/main.go
+++ src/main.go
@@ -1,15 +1,23 @@
 package main
 
 import (
+	"examples.com/controller"
+	"examples.com/service"
 	"github.com/gin-gonic/gin"
+)
+
+var (
+	videoService    service.VideoServices      = service.New()
+	videoController controller.VideoController = controller.New(videoService)
 )
 
 func main() {
 	server := gin.Default()
-	server.GET("/test", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Wrote my first Go server code",
-		})
+	server.GET("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.FindAll())
+	})
+	server.POST("/videos", func(ctx *gin.Context) {
+		ctx.JSON(200, videoController.Save(ctx))
 	})
 	server.Run(":3000")
 }
