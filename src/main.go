64795bf139085fd76b213e8d81cfacf636fda7de
Leksyking <gbemilekeogundipe@gmail.com>
2022-03-09 17:11:31 -0800

    set up auhorization and debugger

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"os"
 
+	gindump "github/tpkeeper/gin-dump"
+
 	"examples.com/controller"
 	"examples.com/middlewares"
 	"examples.com/service"
@@ -22,7 +24,7 @@ func setUpLog() {
 func main() {
 	setUpLog()
 	server := gin.New()
-	server.Use(gin.Recovery(), middlewares.Logger())
+	server.Use(gin.Recovery(), middlewares.Logger(), middlewares.BasicAuth(), gindump.Dump())
 
 	server.GET("/videos", func(ctx *gin.Context) {
 		ctx.JSON(200, videoController.FindAll())
