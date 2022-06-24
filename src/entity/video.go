e63625dec611cadc2771945ba30a4b164d51067b
Leksyking <gbemilekeogundipe@gmail.com>
2022-06-23 23:09:51 -0700

    remove src folder

--- src/entity/video.go
+++
@@ -1,8 +0,0 @@
-package entity
-
-type Video struct {
-	//hows videos should be stored
-	Title       string `json: "title"`
-	Description string `json: "description"`
-	URL         string `json: "url"`
-}
